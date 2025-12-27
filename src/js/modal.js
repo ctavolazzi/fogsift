5f34c9a8877fd06eea983d356266b109ee316fea
Christopher Tavolazzi <ctavolazzi@gmail.com>
2025-12-26 18:38:26 -0800

    chore: Whitespace cleanup in modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -14,7 +14,7 @@ const Modal = {
 
     async loadArticles() {
         if (this.articles) return this.articles;
-        
+
         try {
             const response = await fetch(this.articlesPath);
             const data = await response.json();
